f3c2bc3fccb5d86b532ca70067cdb83365fed513
deadok22 <deadok22@deadok22-1215P.(none)>
2012-10-08 20:31:07 +0400

    added a simple example of a plugin that changes ui settings to playground/deadok22

---
+++ playground/deadok22/ToolWindowSample/src/ru/spbau/idea_cmdline/tool_window_sample/HelloWorldToolWindowFactory.java
@@ -0,0 +1,11 @@
+package ru.spbau.idea_cmdline.tool_window_sample;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: deadok22
+ * Date: 10/8/12
+ * Time: 5:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldToolWindowFactory {
+}

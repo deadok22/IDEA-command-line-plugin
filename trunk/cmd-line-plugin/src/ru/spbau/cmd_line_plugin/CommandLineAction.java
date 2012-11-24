6a19e4dc9c01b3d44ae93d738ce10c2ee561f1be
Sergey A. Savenko <deadok22@gmail.com>
2012-11-24 20:02:07 +0400

    removed all ToolWindow-related stuff from previous version; added CommandLineAction

---
+++ trunk/cmd-line-plugin/src/ru/spbau/cmd_line_plugin/CommandLineAction.java
@@ -0,0 +1,13 @@
+package ru.spbau.cmd_line_plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Author: Sergey A. Savenko
+ */
+public class CommandLineAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
--- trunk/cmd-line-plugin/src/ru/spbau/cmd_line_plugin/interfaces/CommandExecutor.java
+++
@@ -1,8 +0,0 @@
-package ru.spbau.cmd_line_plugin.interfaces;
-
-/**
- * Author: Sergey A. Savenko
- */
-public interface CommandExecutor {
-    boolean executeCommand(Command command);
-}

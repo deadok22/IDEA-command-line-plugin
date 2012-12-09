fef17d9ec8758cdf0183d90fc8d1680e68ad8c77
Sergey A. Savenko <deadok22@gmail.com>
2012-12-09 16:36:13 +0400

    moved extension name definition to other file

--- playground/deadok22/experiments/MyExtendablePlugin/src/extendable_plugin/ExtendablePluginEP.java
+++ playground/deadok22/experiments/MyExtendablePlugin/src/extendable_plugin/ExtendablePluginEP.java
@@ -1,13 +1,9 @@
 package extendable_plugin;
 
-import com.intellij.openapi.extensions.ExtensionPointName;
-
 /**
  * Author: Sergey A. Savenko
  */
 public interface ExtendablePluginEP {
-
-    ExtensionPointName<ExtendablePluginEP> EP_NAME = ExtensionPointName.create("MyExtendablePlugin.WhoAreYouEP");
 
     public String whoAreYou();
 
--- playground/deadok22/experiments/MyExtendablePlugin/src/extendable_plugin/PrintExtensionsAction.java
+++ playground/deadok22/experiments/MyExtendablePlugin/src/extendable_plugin/PrintExtensionsAction.java
@@ -2,6 +2,7 @@ package extendable_plugin;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 
@@ -9,8 +10,11 @@ import com.intellij.openapi.ui.popup.JBPopupFactory;
  * Author: Sergey A. Savenko
  */
 public class PrintExtensionsAction extends AnAction {
+
+    static ExtensionPointName<ExtendablePluginEP> EP_NAME = ExtensionPointName.create("MyExtendablePlugin.WhoAreYouEP");
+
     public void actionPerformed(AnActionEvent e) {
-        final ExtendablePluginEP[] exts = (ExtendablePluginEP[])Extensions.getExtensions(ExtendablePluginEP.EP_NAME);
+        final ExtendablePluginEP[] exts = (ExtendablePluginEP[])Extensions.getExtensions(EP_NAME);
         String message = "extensions:\n";
         for (ExtendablePluginEP ext : exts) {
             message += ext.whoAreYou();
